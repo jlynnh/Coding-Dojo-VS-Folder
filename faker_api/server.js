0f58a364706559b39745ecf4d5f834f9ea6810ed
Jennifer <121691049+jlynnh@users.noreply.github.com>
2023-05-05 18:44:33 -0700

    Faker Assignment

---
+++ faker_api/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.send(newFakeUser, newFakeCompany);
+});
+
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:${port}');
+});
+
+const faker = require('@faker-js/faker');
+const createUser = () => {
+    const newUser = {
+        password: faker.internet.password(20, true, [A-Z]),
+        email: faker.internet.exampleEmail({allowSpecialCharacters: true }),
+        phoneNumber: faker.phone.number(),
+        lastName: faker.name.lastName(),
+        firstName: faker.name.firstName(),
+        _id: faker.datatype.uuid()
+    };
+    return newUser;
+};
+
+const newFakeUser = newUser();
+console.log(newFakeUser);
+
+const createCompany = () => {
+    const newCompany = {
+        _id: faker.datatype.uuid(),
+        name: faker.company.companyName(),
+        street: faker.address.streetAddress(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        zipCode: faker.address.zipCodeByState(),
+        country: faker.address.country()
+    };
+    return newCompany;
+};
+
+const newFakeCompany = newCompany();
+console.log(newFakeCompany);
+
+
