ddec6152390b4a13452030b215a8c6439e272408
Jennifer <121691049+jlynnh@users.noreply.github.com>
2023-05-14 15:43:19 -0700

    v2 Commit for Product assignment

---
+++ Product_Manager/client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import form from './components/form';
+
+function App() {
+  return (
+    <div className="App">
+      <form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Product_Manager/client/src/components/form.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+
+const form = () => {
+    
+  return (
+    <div>form</div>
+  )
+}
+
+export default form
