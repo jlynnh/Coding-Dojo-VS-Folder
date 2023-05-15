b292bfefefed4d23d96db888f1d25b9c5daeae8e
Jennifer <121691049+jlynnh@users.noreply.github.com>
2023-05-14 17:14:03 -0700

    v3 commit

--- Product_Manager/client/src/components/Productform.js
+++ Product_Manager/client/src/components/Productform.js
@@ -2,33 +2,29 @@
 import React, {useState} from 'react'
 import axios from 'axios'
 
-const dataStyle = {
-    borderRadius: "0px",
-    backgroundColor: "white",
-    border: "1px solid darkgrey",
-    padding: "20px",
-    margin: "20px",
-    width: "700px"
-}
-const Form = (props) => {
+const ProductForm = () => {
     const [title, setTitle] = useState("")
     const [price, setPrice] = useState(Number)
     const [description, setDescription] = useState("")
 
-const createProduct = (e) => {
+const onSubmitHandler = (e) => {
     e.preventDefault();    
-        const newProduct = { 
-            title: title, 
-            price: price, 
-            description: description 
-        };
-        console.log(newProduct);
-    };
+    axios.post('http://localhost:8000/api/product', {
+        title,
+        price,
+        description
+    })
+        .then(res=>{
+            console.log(res); 
+            console.log(res.data);
+        })
+        .catch(err=>console.log(err))
+}
 
     return (
         <div>
             <div className='row'>
-                <form action='' className='form-inline' onSubmit={createProduct}>
+                <form action='' className='form-inline' onSubmit={onSubmitHandler}>
                     <div className='form-group'>
                         <label htmlFor='title'className='data'>Title:</label>
                         <input type='text' name='title' id='title' className='form-control' onChange={ (e) => setTitle(e.target.value) }/>
@@ -41,7 +37,7 @@ const createProduct = (e) => {
                         <label htmlFor='description'className='data'>Description:</label>
                         <input type='text' name='description' id='description' className='form-control' onChange={ (e) => setDescription(e.target.value) }/>
                     </div>
-                    <button htmlFor='button' className='button'>Create</button>
+                    <button htmlFor='button' className='button' onClick={()=> alert("Product has been created!")}>Create</button>
                     <input type="submit" value="Create Product" />
                 </form>
             </div>
